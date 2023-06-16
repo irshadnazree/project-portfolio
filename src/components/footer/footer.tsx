c4a3eb2268fe68051ef85ef3a83bd692154bdaf6
Irshad Nazree <irshad@hiredly.com>
2023-06-16 21:22:49 +0800

    feat: add dark mode toggle

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,16 @@
-import { component$ } from '@builder.io/qwik';
+import { PropFunction, component$ } from '@builder.io/qwik';
 
-export default component$(() => {
+interface FooterProps {
+  isDarkMode: boolean;
+  changeMode: PropFunction<(value: boolean) => void>;
+}
+
+export default component$((props: FooterProps) => {
   return (
     <footer>
-      <button>dark</button>
-      <button>light</button>
+      <button onClick$={() => props.changeMode(!props.isDarkMode)}>
+        {props.isDarkMode ? 'Dark' : 'Light'}
+      </button>{' '}
     </footer>
   );
 });
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,18 +1,23 @@
-import { component$, Slot, useSignal } from '@builder.io/qwik';
+import { component$, Fragment, Slot, useSignal, $ } from '@builder.io/qwik';
 import Footer from '~/components/footer/footer';
 import Header from '~/components/header/header';
 
 export default component$(() => {
-  const currentPage = useSignal('about');
-  const darkMode = useSignal(true);
+  // const currentPage = useSignal('about');
+  const isDarkMode = useSignal(true);
+
+  const changeMode = $((value: boolean) => {
+    isDarkMode.value = value;
+    console.log(isDarkMode.value);
+  });
 
   return (
-    <>
+    <Fragment>
       <Header />
       <main>
         <Slot />
       </main>
-      <Footer />
-    </>
+      <Footer isDarkMode={isDarkMode.value} changeMode={changeMode} />
+    </Fragment>
   );
 });
